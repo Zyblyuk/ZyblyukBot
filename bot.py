5aaaa7115e037f0aac7ed9c2493f970b1a92800f
Zyblyuk <qikroman@gmail.com>
2020-07-07 22:50:34 +0300

    created a command /currencies. This command shows the ratio of some currencies to the dollar

--- bot.py
+++ bot.py
@@ -1,8 +1,27 @@
+import telebot, requests, random
+from telebot import types
+from bs4 import BeautifulSoup
 from save_and_load import Users
 from Token import token
-import telebot
-from telebot import types
 
+
+bot = telebot.TeleBot(token)
+iter = 0
+List = Users("DataBase")
+print(List.users)
+
+def monney(URL):
+    Dolar_rub = URL
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+    full_page = requests.get(Dolar_rub, headers=headers)
+    soup = BeautifulSoup(full_page.content, 'html.parser')
+    convert = soup.findAll("span", {"class": "converter-100__info-currency-bold"})
+    string = ''
+    for i in convert[0].text:
+        if i == ' ':
+            break
+        string += i
+    return string
 
 def cut(data, begin, end):
     list = {}
@@ -23,9 +42,7 @@ def next(list):
         callback_button = types.InlineKeyboardButton(text=string, callback_data=key)
         keyboard.add(callback_button)
     return keyboard
-iter = 0
-List = Users("DataBase")
-print(List.users)
+
 def push_back_List(message):
     global List
     if not message.chat.id in List.users:
@@ -33,8 +50,6 @@ def push_back_List(message):
     elif List.users[message.chat.id] != [message.from_user.first_name, message.from_user.last_name, message.from_user.username] :
             List.users[message.chat.id] = [message.from_user.first_name, message.from_user.last_name, message.from_user.username]
             List.save()
-
-bot = telebot.TeleBot(token)
 
 def return_String (message):
     String = ''
@@ -73,10 +88,23 @@ def take_control(message):
         keyboard.add(next_list_button)
     bot.send_message(message.chat.id, "Выбери ключ:", reply_markup=keyboard)
 
+@bot.message_handler(commands=['currencies'])
+def currencies(message):
+    keyboard = types.InlineKeyboardMarkup()
+    curr_bel = types.InlineKeyboardButton(text= monney('https://myfin.by/converter/usd-byn/1') + " бел. руб.", callback_data="много")
+    curr_eur = types.InlineKeyboardButton(text= monney('https://myfin.by/converter/usd-eur/1') + " евро", callback_data="много")
+    curr_rub = types.InlineKeyboardButton(text= monney('https://myfin.by/converter/usd-rub/1') + " рус.руб.", callback_data="много")
+    keyboard.add(curr_eur, curr_rub)
+    keyboard.add(curr_bel)
+    bot.send_message(message.chat.id, "Один доллар равен:", reply_markup=keyboard)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     global List
+    if call.data == "много":
+        answer = ["Кааапец, дa???", "уффф", "........", "дааа, куда катится этот мир...", "все из-за Гейца!!!", "фу, воняет!", "@#$%^& рот!!!"]
+        bot.send_message(call.message.chat.id, random.choice(answer))
     if call.data in List.users:
         print(List.users[call.data])
     if call.data == "Next":
@@ -87,7 +115,7 @@ def callback_worker(call):
         keyboard.add(types.InlineKeyboardButton(text="Next", callback_data="next"))
 
 @bot.message_handler(commands=['list_clear'])
-def take_control(message):
+def list_clear(message):
     if message.chat.id != 422752846 :
         bot.send_message(422752846, "Это комманда только для админа, ты не админ!")
         return
